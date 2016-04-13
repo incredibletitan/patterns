2aff03d1d12031b9e7802d0d51daea206f16ea77
Yuriy Stos <100sbsh@gmail.com>
2016-04-13 14:06:06 +0300

    Added dummy creation of Lesson and Seminar with timed and fixed price cost strategies

--- strategy/index.php
+++ strategy/index.php
@@ -2,6 +2,18 @@
 require_once __DIR__ . "/../bootstrap.php";
 
 use strategy\libs\TimedCostStrategy;
+use strategy\libs\FixedPriceCostStrategy;
 use strategy\libs\Lecture;
+use strategy\libs\Seminar;
+use strategy\libs\Lesson;
 
-echo (new Lecture(16, new TimedCostStrategy()))->cost();
+$lessons = [];
+$lessons[] = new Lecture(16, new TimedCostStrategy());
+$lessons[] = new Seminar(10, new FixedPriceCostStrategy());
+
+foreach ($lessons as $lesson) {
+    if ($lesson instanceof Lesson) {
+        echo 'Price :' . $lesson->cost() . '<br/>';
+        echo 'Charge type:' . $lesson->chargeType() . '<br/>';
+    }
+}
