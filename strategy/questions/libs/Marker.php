f89cf22a8b9d0a29300b3835b4c7d499f8464705
Yuriy Stos <100sbsh@gmail.com>
2016-09-06 14:19:42 +0300

    Added constructor and comments for Marker class

--- strategy/questions/libs/Marker.php
+++ strategy/questions/libs/Marker.php
@@ -9,5 +9,25 @@ namespace strategy\questions\libs;
  */
 abstract class Marker
 {
-    abstract public function mark();
+    /**
+     * @var $test - Template with which marker will be compared
+     */
+    protected $test;
+
+    /**
+     * Marker constructor.
+     * @param $test
+     */
+    public function __construct($test)
+    {
+        $this->test = $test;
+    }
+
+    /**
+     * Compare $this->test and $response variables by specific algorithm needed to be implemented in this method
+     *
+     * @param $response
+     * @return mixed
+     */
+    abstract public function mark($response);
 }
