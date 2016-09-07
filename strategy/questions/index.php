64f39b0c4d55271cc42f39a0107d9ebb68eaa7ce
Yuriy Stos <100sbsh@gmail.com>
2016-09-07 13:10:29 +0300

    Finished marking functionality

--- strategy/questions/index.php
+++ strategy/questions/index.php
@@ -1,2 +1,17 @@
 <?php
 require_once __DIR__ . "/../../bootstrap.php";
+
+use strategy\questions\libs\Question;
+use strategy\questions\libs\RegexpMarker;
+use strategy\questions\libs\MatchMarker;
+use strategy\questions\libs\MarkLogicMarker;
+
+$markers = [
+    new RegexpMarker('/.s/'),
+    new MarkLogicMarker('ok'),
+    new MatchMarker('ok')
+];
+
+foreach ($markers as $marker) {
+    echo get_class($marker) . " : " . ((new Question('How are you do?', $marker))->mark('ok') ? 'ok' : 'fail');
+}
