eb0195ce67cf8626fc46eba4c8b3e79728864a87
Yuriy <100sbsh@gmail.com>
2017-11-23 21:27:26 +0200

    Added some explanation for Observer pattern code example

--- code_examples/observer/index.php
+++ code_examples/observer/index.php
@@ -1,7 +1,11 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
-
+//Creating a new DayBook
 $dayBook = new \libs\DayBook();
+
+//Added listeners of marks in the daybook
 $dayBook->attach(new \libs\FatherObserver());
 $dayBook->attach(new \libs\MotherObserver());
+
+//Putting mark
 $dayBook->putMark(new \libs\Mark('Math', 5));
